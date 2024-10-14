f5984e79c59fa6c2e4db85ed60fc79a5c1e9db48
mjmj1992 <michal.jablonski1992@gmail.com>
2024-10-14 16:21:43 +0200

    clerk/convex config added; loading logo amends; favicon added; dummy homepage added; shadcn theme added;

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  compiler: {
+    removeConsole: process.env.NODE_ENV !== "development",
+  },
+  swcMinify: true, // Better performance than Terser
+  images: {
+    domains: ["img.clerk.com", "utfs.io"],
+  },
+  async redirects() {
+    return [{ source: "/", destination: "/home", permanent: true }];
+  },
+};
 
 export default nextConfig;
