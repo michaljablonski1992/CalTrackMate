c6de64b4ef5dff7c9b0eb2543d79a08c8c12ddcb
mjmj1992 <michal.jablonski1992@gmail.com>
2024-10-21 18:49:26 +0200

    jest config to match tests only added;

--- jest.config.ts
+++ jest.config.ts
@@ -14,6 +14,10 @@ const config: Config = {
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/$1', // Map @/ to the root directory
   },
+  testMatch: [
+    "**/__tests__/**/*.test.[jt]s?(x)",  // Match .test.js, .test.ts, and .test.tsx files in __tests__ folders
+    "**/?(*.)+(test).[jt]s?(x)",         // Match files with .test.js, .test.ts, and .test.tsx anywhere in the project
+  ],
   // Add more setup options before each test is run
   // setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 }
