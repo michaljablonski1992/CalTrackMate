584359b23979d056a3e50b25bab99dd62ab2313a
mjmj1992 <michal.jablonski1992@gmail.com>
2024-11-06 08:07:49 +0100

    DailyLog component cleanup

--- app/home/_components/DailyLog/DailyLog.tsx
+++ app/home/_components/DailyLog/DailyLog.tsx
@@ -1,16 +1,16 @@
 'use client';
 
 import { useFoodContext } from '@/context/FoodContext';
-import { FatsecretFood, FatsecretServing, foodDisplayName } from '@/lib/fatsecret/api';
+import { FatsecretFood, FatsecretServing } from '@/lib/fatsecret/api';
 import { FileClockIcon, ScrollIcon } from 'lucide-react';
 import { ScrollArea } from '@/components/ui/scroll-area';
 import CardWrapper from '@/components/shared/CardWrapper';
 import CardInfo from '@/components/shared/CardInfo';
 import Spinner from '@/components/shared/Spinner';
 import DailyLogLabel from './DailyLogLabel';
-import DailyLogRemoveIcon from './DailyLogRemoveIcon';
 import { useState } from 'react';
 import RemoveRecordDialog from './RemoveRecordDialog';
+import DailyLogFood from './DailyLogFood';
 
 export type itemToDeleteTypes = [] | [FatsecretFood, FatsecretServing?];
 
@@ -58,29 +58,8 @@ export default function DailyLog() {
         {!foodCtx.foodsLoading && foodCtx.foods.length > 0 && (
           <ScrollArea type="always">
             <ul className="list-disc ml-5">
-              {foodCtx.foods.map((food, index) => (
-                <li key={index} className="mb-2 items-center">
-                  <span className="flex gap-2">
-                    <span className="font-bold">{foodDisplayName(food)}</span>{' '}
-                    <DailyLogRemoveIcon ariaLabel="Remove food" className="w-5 h-5 mb-2" onClickHandler={() => removeRecordClickHandler(food)} />
-                  </span>
-                  <ul className="leading-6">
-                    {food.servings.serving.map((serving) => {
-                      return (
-                        <li
-                          key={serving.serving_id}
-                          className="ml-2 flex gap-2"
-                        >
-                          -{' '}
-                          <span className="font-bold text-bold-muted">
-                            {serving.serving_description} x {serving.quantity}
-                          </span>
-                          <DailyLogRemoveIcon ariaLabel="Remove serving" className="w-4 h-4 mb-1" onClickHandler={() => removeRecordClickHandler(food, serving)} />
-                        </li>
-                      );
-                    })}
-                  </ul>
-                </li>
+              {foodCtx.foods.map((food) => (
+                <DailyLogFood key={food.food_id} food={food} removeRecordClickHandler={removeRecordClickHandler}/>
               ))}
             </ul>
           </ScrollArea>
