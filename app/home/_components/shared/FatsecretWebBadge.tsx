0af81a3cd14f3389773d80adb56e3bcb2dad5792
mjmj1992 <michal.jablonski1992@gmail.com>
2024-11-14 17:59:59 +0100

    fatsecret attribution web badge added build fix;

--- app/home/_components/shared/FatsecretWebBadge.tsx
+++ app/home/_components/shared/FatsecretWebBadge.tsx
@@ -5,7 +5,6 @@ const FatsecretWebBadge = () => {
       <a href="https://www.fatsecret.com">
         <img
           src="https://platform.fatsecret.com/api/static/images/powered_by_fatsecret.svg"
-          border="0"
           alt="Powered by fatsecret"
         />
       </a>
